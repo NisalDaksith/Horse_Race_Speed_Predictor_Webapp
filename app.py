5d9c74a4826f5f032d5bae3562235fa963d49b70
NisalDaksith <nisaldaksith1999@gmail.com>
2022-11-01 16:33:45 +0530

    Nothing important was changed

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ import streamlit
 from reg_predict_page import show_predict_page
 
 show_predict_page()
+
--- reg_predict_page.py
+++ reg_predict_page.py
@@ -16,3 +16,11 @@ config = data["config"]
 def show_predict_page():
     st.title("Horse Speed Predictor")
     st.write("""### First we need some information about your horse and the Track""")
+    
+    horse_type = ("Gelding","Brown","Horse","Mare","Colt","Steed","Mount")
+    config = ("A","B","B+2","C","C+3")
+    horse_age = ("2","3","4","5","6","7","8")
+
+    horse_type = st.selectbox("Type of your Horse: ",horse_type)
+    config = st.selectbox("Configuration of the Track: ",config)
+    horse_age = st.selectbox("Age of the Horse: ",)
