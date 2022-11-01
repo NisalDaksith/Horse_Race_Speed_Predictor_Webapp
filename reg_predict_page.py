f3b48fac504c9534e03bf40e82639eca03dd70df
NisalDaksith <nisaldaksith1999@gmail.com>
2022-11-02 01:51:24 +0530

    Model is fully complete and responsive

--- reg_predict_page.py
+++ reg_predict_page.py
@@ -1,6 +1,10 @@
 import streamlit as st 
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+from sklearn.preprocessing import LabelEncoder
 
 def load_model():
     
@@ -19,23 +23,26 @@ def show_predict_page():
     st.write("""### First we need some information about your horse and the Track""")
     
     horse_type = ("Gelding","Brown","Horse","Mare","Colt","Steed","Mount")
-    config = ("A","B","B+2","C","C+3")
-    horse_age = (["2","3","4","5","6","7","8"])
-    surface = (["0","1"])
+    track_config = ("A","B","B+2","C","C+3")
+    horse_age = ([2,3,4,5,6,7,8])
+    surface = ([0,1])
     
     horse_type = st.selectbox("Type of your Horse ",horse_type)
-    config = st.selectbox("Configuration of the Track ",config)
+    track_config = st.selectbox("Configuration of the Track ",track_config)
     horse_age = st.selectbox("Age of the Horse ",horse_age)
     surface = st.selectbox("Type of the Surface( 0 = Dirt , 1 = Turf ) ",surface)
     horse_weight = st.slider("Weight of the Horse ",0,1200)                #Could be problematic 
     rider_weight = st.slider("Weight of the Rider ",0,150)                 #Could be problematic
     
+    
+    
     ok = st.button("Predict Speed")
     if ok:
-        X = np.array([[horse_type,config,horse_age,horse_weight,rider_weight,surface]])
+        X = np.array([[horse_type,track_config,horse_age,horse_weight,rider_weight,surface]])
         X[:,0] = htype.transform(X[:,0])
         X[:,1] = config.transform(X[:,1])
+    
         X = X.astype(float)
         
         horse_speed = linearReg.predict(X)
-        st.subheader(f"The Predicted Speed is ${horse_speed[0]:.4f} meters per second.")
+        st.subheader(f"The Predicted Speed is {horse_speed[0]:.4f} meters per second.")
